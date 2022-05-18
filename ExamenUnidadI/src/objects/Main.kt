177e8fed454cf81d433f94e1489cc4caee5bd017
20193tn024@utez.edu.mx <20193tn024@utez.edu.mx>
2022-05-18 17:58:45 -0500

    Final

--- ExamenUnidadI/src/objects/Main.kt
+++ ExamenUnidadI/src/objects/Main.kt
@@ -1,8 +1,38 @@
 package objects
 
+import objects.Main.Companion.MENSAJE_ERROR_ADD
+import objects.Main.Companion.MENSAJE_ERROR_UPDATE
+import objects.Main.Companion.MENSAJE_SUCCES_ADD
+import objects.Main.Companion.MENSAJE_SUCCES_UPDATE
 import java.util.*
 
 class Main {
+    //Mensajes (error, succes, info)
+    companion object {
+
+        //CookingCourse, ProgramingCourse
+        //Add
+        const val MENSAJE_SUCCES_ADD = "El curso  ha sido agregado correctamente."
+        const val MENSAJE_ERROR_ADD = "Error, el elemento no ha sido agreagado."
+        const val MENSJE_INFO_ADD = "Es posible que el elemento no haya sido agregado."
+
+        //Update
+        const val MENSAJE_SUCCES_UPDATE = "El curso ha sido actualizado correctamente."
+        const val MENSAJE_ERROR_UPDATE = "Error, el elemento no pudo ser actualizado."
+        const val MENSJE_INFO_UPDATE = "Es posible que el elemento no haya sido actualizado."
+
+        //Eliminar
+        const val MENSAJE_SUCCES_ELIMINAR = "El curso ha sido eliminado correctamente."
+        const val MENSAJE_ERROR_ELIMINAR = "Error, el elemento no ha sido eliminado."
+        const val MENSJE_INFO_ELIMINAR = "Es posible que el elemento no haya sido eliminado."
+
+
+        //Get
+        const val MENSAJE_SUCCES_GET = "El curso ha sido consultado correctamente."
+        const val MENSAJE_ERROR_GET = "Error, el elemento no ha sido consultado."
+        const val MENSJE_INFO_GET = "Es posible que el elemento no haya sido consultado."
+    }
+
     val sc: Scanner = Scanner(System.`in`);
     var cookingCourses: MutableList<CookingCourse> = mutableListOf()
     var programingCourses: MutableList<ProgramingCourse> = mutableListOf()
@@ -15,7 +45,7 @@ class Main {
             println("Ingresa el nombre del curso: ")
             name = sc.nextLine()
             var comparison: Boolean = (getOneprogramingCourse(name) != null)
-            if (comparison) println("Ese nombre ya existe")
+            if (comparison) println(MENSJE_INFO_ADD)
         } while (comparison)
         println("Ingresa la descripción del curso: ")
         var description = sc.next()
@@ -29,8 +59,8 @@ class Main {
         do {
             println("Ingresa el nombre del curso: ")
             name = sc.nextLine()
-            var comparison : Boolean = (getOneCookingCourse(name) != null)
-            if (comparison) println("Ese nombre ya existe")
+            var comparison: Boolean = (getOneCookingCourse(name) != null)
+            if (comparison) println(MENSJE_INFO_ADD)
         } while (comparison)
         println("Ingresa la descripción del curso: ")
         var description = sc.next()
@@ -88,6 +118,60 @@ class Main {
         }
         return null
     }
+
+    fun getOneCookingCourseById(id: Int): Int {
+        for (cookingCourse in cookingCourses) {
+            if (cookingCourse.id.equals(id))
+                return cookingCourses.indexOf(cookingCourse)
+        }
+        return 0
+    }
+
+    fun getOneprogramingCourseById(id: Int): Int {
+        for (programingCourse in programingCourses) {
+            if (programingCourse.id == id)
+                return programingCourses.indexOf(programingCourse)
+        }
+        return 0
+    }
+    fun getAll() {
+        for (cookingCourse in cookingCourses) {
+            println("Curso : ${cookingCourse.name} ${cookingCourse.description}")
+        }
+        for (programingCourse in programingCourses) {
+            println("Curso : ${programingCourse.name} ${programingCourse.description}")
+        }
+    }
+
+    fun getOneCookingCourseById2(id: Int): CookingCourse? {
+        for (cookingCourse in cookingCourses) {
+            if (cookingCourse.id.equals(id))
+                return cookingCourse
+        }
+        return null
+    }
+
+    fun removeCookingCourse(id: Int) {
+        var course = getOneCookingCourseById2(id)
+        if (course != null) {
+            cookingCourses.remove(course)
+        }
+    }
+    fun getOneProgramingCourseById2(id: Int): ProgramingCourse? {
+        for (programingCourses in programingCourses) {
+            if (programingCourses.id.equals(id))
+                return programingCourses
+        }
+        return null
+    }
+
+    fun removeProgramingCourse(id: Int) {
+        var course = getOneProgramingCourseById2(id)
+        if (course != null) {
+            programingCourses.remove(course)
+        }
+    }
+
 }
 
 fun main(args: Array<String>) {
@@ -116,18 +200,24 @@ fun main(args: Array<String>) {
                     when (opc) {
                         1 -> {
                             course = main.addCookingCourse()
-                            if (course != null) main.cookingCourses.add(course) else println(main.companionObj)
+                            if (course != null) {
+                                main.cookingCourses.add(course)
+                                println(MENSAJE_SUCCES_ADD)
+                            } else println(MENSAJE_ERROR_ADD)
                         }
                         2 -> {
                             println("Ingresa el nombre del curso que desea actualizar")
                             course = main.updateCookingCourse(main.sc.next())
-
+                            if (course != null) {
+                                main.getOneCookingCourseById(course.id)
+                                println(MENSAJE_SUCCES_UPDATE)
+                            } else println(MENSAJE_ERROR_UPDATE)
                         }
                         3 -> {
-
+                            println("Ingresa el id")
+                            var id = main.removeCookingCourse(main.sc.nextInt())
                         }
                         4 -> {
-
                         }
                         else -> println("Opción inválida")
                     }
@@ -147,17 +237,20 @@ fun main(args: Array<String>) {
                     when (opc) {
                         1 -> {
                             course = main.addProgramingCourse()
-                            if (course != null) main.programingCourses.add(course) else println()
+                            if (course != null) {
+                                val index = main.programingCourses.add(course)
+                                println(MENSAJE_SUCCES_ADD)
+                            } else println(MENSAJE_ERROR_ADD)
                         }
                         2 -> {
                             println("Ingresa el nombre del curso que desea actualizar")
                             course = main.updateProgramingCourse(main.sc.next())
                         }
                         3 -> {
-
+                            println("Ingresa el id")
+                            var id = main.removeProgramingCourse(main.sc.nextInt())
                         }
                         4 -> {
-
                         }
                         else -> println("Opción inválida")
                     }
