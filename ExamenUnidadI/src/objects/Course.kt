ec287c19ae7ba1a6879e65b305ed0a2ec695b751
NoemiAra <20193tn004@utez.edu.mx>
2022-05-18 16:05:59 -0500

    Añadiendo course

---
+++ ExamenUnidadI/src/objects/Course.kt
@@ -0,0 +1,19 @@
+open class Course (var id: Int, var name: String, var description: String){
+
+    open fun add():String {
+
+    }
+
+    open fun update(): Int {
+
+    }
+
+    open fun remove():Int {
+
+    }
+
+    open fun get(){
+
+    }
+
+}
